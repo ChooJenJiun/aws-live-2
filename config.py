3fe37d87bf5546fc1211fafd78d30736ecfd6007
ChooJenJiun <101780475+ChooJenJiun@users.noreply.github.com>
2022-04-10 20:52:08 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cldnotqrt2jn.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "s3-choojenjiun"
+customregion = "us-east-1"
